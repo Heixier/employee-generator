ab8c9074c2e25fa4631a0b8934f6f0afcd3f566c
Heixier <rzsiah@gmail.com>
2024-02-19 14:02:44 +0800

    did some optimisation

--- DDMG/employees_generator.py
+++ DDMG/employees_generator.py
@@ -1,36 +1,40 @@
 import random
 import employees
+from pathlib import Path
+
+path = Path(__file__).parents[0]
+output_file = "employees.txt"
 
 names = []
+shuffled_names = []
 first_names = []
 last_names = []
 
-with open("first_names.txt", "r") as f:
-    first_names = f.read().splitlines()
-    
-# random.shuffle(first_names)
+with open(f"{path}/names.txt", "r") as f:
+    names = f.read().splitlines()
 
-with open("last_names.txt", "r") as f:
-    last_names = f.read().splitlines()
-    
-# random.shuffle(last_names)
-
-# for first_name, last_name in zip(first_names, last_names):
-#     names.append(first_name + " " + last_name)
+for name in names:
+    name_parts = name.split(" ")
+    first_names.append(name_parts[0])
+    last_names.append(name_parts[1])
 
 amount = int(input("How many employees do you want to generate? "))
+
+while amount > 10000:
+    amount = int(input("Total exceeds 4 digits! Try another number: "))
 
 for name in range(amount):
     first_name = random.choice(first_names)
     last_name = random.choice(last_names)
     
-    names.append(first_name + " " + last_name)
+    shuffled_names.append(first_name + " " + last_name)
 
-with open("employees.txt", "w") as f:
+with open(f"{path}/{output_file}", "w+") as f:
     f.write("INSERT INTO employees\nVALUES\n")
-    for x, name in enumerate(names):
+    for x, name in enumerate(shuffled_names):
         
         empid = format(x, '04d')
         employee = employees.Employees(empid, name)
         f.write(f'("E{employee.id}", "{employee.name}", {employee.departmentid}, "{employee.department}", {employee.wage}),\n')
         
+print(f"Generated {len(shuffled_names)} names!\nResults stored in \"{path}\" under \"{output_file}\"")
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # employee-generator
 Employee generator for SQL database testing
+
+## Instructions ##
+Download the .zip folder
+
+Run employees_generator.py
+
+Find the output file stored under "employees.txt"
+
+Paste the contents of the file into your SQL query window
